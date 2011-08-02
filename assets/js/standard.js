13caa3e880c29845a36daf7956ff503dbaaad738
Zozi <zozfried@users.noreply.github.com>
2011-08-02 13:40:35 +0200

    refactored the form styles to make them overridable without braking your brain

---
+++ assets/js/standard.js
@@ -0,0 +1,603 @@
+/**
+ * SallyCMS - JavaScript-Bibliothek
+ */
+
+var sly = {};
+
+// do not use dots inside callback names
+var slyLinkWidgetCallback  = null;
+var slyMediaWidgetCallback = null;
+
+(function($, sly, undef) {
+	var imageExtensions = ['png', 'gif', 'jpg', 'jpeg', 'bmp'];
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Popups
+
+	var openPopups = [];
+
+	sly.Popup = function(name, url, posx, posy, width, height, extra) {
+		this.name = name;
+		this.url  = url;
+		this.obj  = window.open(url, name, 'width='+width+',height='+height+extra);
+
+		this.obj.moveTo(posx, posy);
+		this.obj.focus();
+
+		openPopups[name] = this;
+
+		this.close = function() {
+			this.obj.close();
+		};
+
+		this.setGlobal = function(name, value) {
+			this.obj[name] = value;
+		};
+	};
+
+	sly.closeAllPopups = function() {
+		for (var name in openPopups) {
+			if (openPopups.hasOwnProperty(name)) {
+				openPopups[name].close();
+			}
+		}
+	};
+
+	sly.openCenteredPopup = function(name, link, width, height, extra) {
+		if (width === 0)  width  = 550;
+		if (height === 0) height = 400;
+
+		if (extra === undef) {
+			extra = ',scrollbars=yes,toolbar=no,status=yes,resizable=yes';
+		}
+
+		var posx = parseInt((screen.width-width) / 2, 10);
+		var posy = parseInt((screen.height-height) / 2, 10) - 24;
+
+		return new sly.Popup(name, link, posx, posy, width, height, extra);
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Mediapool
+
+	sly.openMediapool = function(subpage, value, callback) {
+		var url = 'index.php?page=mediapool';
+
+		if (value) {
+			url += '&subpage=detail&file_name='+value;
+		}
+		else if (subpage && (subpage != 'detail' || value)) {
+			url += '&subpage=' + subpage;
+		}
+
+		if (callback) {
+			url += '&callback='+callback;
+		}
+
+		return sly.openCenteredPopup('slymediapool', url, 760, 600);
+	};
+
+	sly.openLinkmap = function(value, callback) {
+		var url = 'index.php?page=linkmap';
+
+		if (value) {
+			url += '&category_id='+value;
+		}
+
+		if (callback) {
+			url += '&callback='+callback;
+		}
+
+		return sly.openCenteredPopup('slylinkmap', url, 760, 600);
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Helper
+
+	var inherit = function(subClass, baseClass) {
+		var tmpClass = function() {};
+		tmpClass.prototype = baseClass.prototype;
+		subClass.prototype = new tmpClass();
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Abstract Widget
+
+	sly.AbstractWidget = function(elem) {
+		this.element    = $(elem);
+		this.valueInput = this.element.find('input[rel=value]');
+		this.nameInput  = this.element.find('input[rel=name]');
+
+		// register events
+		var icons = this.element.find('.sly-icons');
+		icons.delegate('a[rel=open]',   'click', $.proxy(this.onOpen,   this));
+		icons.delegate('a[rel=add]',    'click', $.proxy(this.onAdd,    this));
+		icons.delegate('a[rel=delete]', 'click', $.proxy(this.onDelete, this));
+	};
+
+	sly.AbstractWidget.prototype = {
+		getValue: function() {
+			return this.valueInput.val();
+		},
+
+		setValue: function(identifier, title) {
+			this.valueInput.val(identifier);
+			this.nameInput.val(title);
+			return true; // signalize the popup to close itself
+		},
+
+		clear: function() {
+			this.setValue('', '');
+		},
+
+		onOpen: function() {
+			return false;
+		},
+
+		onAdd: function() {
+			return false;
+		},
+
+		onDelete: function() {
+			this.clear();
+			return false;
+		}
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Media Widgets
+
+	sly.MediaWidget = function(elem) {
+		sly.AbstractWidget.call(this, elem);
+	};
+
+	inherit(sly.MediaWidget, sly.AbstractWidget);
+
+	sly.MediaWidget.prototype.onOpen = function() {
+		sly.openMediapool('detail', this.getValue(), 'slyMediaWidgetCallback');
+		slyMediaWidgetCallback = $.proxy(this.setValue, this);
+		return false;
+	};
+
+	sly.MediaWidget.prototype.onAdd = function() {
+		sly.openMediapool('upload', '', 'slyMediaWidgetCallback');
+		slyMediaWidgetCallback = $.proxy(this.setValue, this);
+		return false;
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Link Widgets
+
+	sly.LinkWidget = function(elem) {
+		sly.AbstractWidget.call(this, elem);
+	};
+
+	inherit(sly.LinkWidget, sly.AbstractWidget);
+
+	sly.LinkWidget.prototype.onOpen = function() {
+		var catID = this.element.data('catid');
+
+		sly.openLinkmap(catID, 'slyLinkWidgetCallback');
+		slyLinkWidgetCallback = $.proxy(this.setValue, this);
+
+		return false;
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Generic lists
+
+	sly.AbstractListWidget = function(elem) {
+		this.element = $(elem);
+		this.input   = this.element.find('input');
+		this.list    = this.element.find('select');
+
+		// register events
+		var icons = this.element.find('.sly-icons.move');
+		icons.delegate('a', 'click', $.proxy(this.onMove, this));
+
+		icons = this.element.find('.sly-icons.edit');
+		icons.delegate('a[rel=open]',   'click', $.proxy(this.onOpen,   this));
+		icons.delegate('a[rel=add]',    'click', $.proxy(this.onAdd,    this));
+		icons.delegate('a[rel=delete]', 'click', $.proxy(this.onDelete, this));
+	};
+
+	sly.AbstractListWidget.prototype = {
+		getElements: function() {
+			var options = this.list.find('option');
+			var result  = [];
+
+			for (var i = 0, len = options.length; i < len; ++i) {
+				result.push(options[i].value);
+			}
+
+			return result;
+		},
+
+		getSelected: function() {
+			var selected = this.list.find('option:selected');
+			return selected.length ? selected.val() : null;
+		},
+
+		clear: function() {
+			this.list.find('option').remove();
+			this.input.val('');
+		},
+
+		addValue: function(identifier, title) {
+			this.list.append($('<option>').val(identifier).text(title));
+			this.createList();
+			return false; // signalize the popup to keep open
+		},
+
+		onDelete: function() {
+			var selected = this.list.find('option:selected');
+
+			if (selected.length === 0) {
+				return false;
+			}
+
+			// find the element to select
+			var toSelect = selected.next();
+
+			if (toSelect.length === 0) {
+				toSelect = selected.prev();
+			}
+
+			// remove element and mark the next/prev one
+			selected.remove();
+			toSelect.prop('selected', true);
+
+			// re-create the <input>
+			this.createList();
+
+			// done
+			return false;
+		},
+
+		onMove: function(event) {
+			var direction = $(event.target).closest('a').attr('rel');
+			var list      = this.list;
+			var selected  = list.find('option:selected');
+
+			if (selected.length === 0) {
+				return false;
+			}
+
+			if (direction === 'top') {
+				list.prepend(selected);
+			}
+			else if (direction === 'up') {
+				selected.prev().insertAfter(selected);
+			}
+			else if (direction === 'down') {
+				selected.next().insertBefore(selected);
+			}
+			else if (direction === 'bottom') {
+				list.append(selected);
+			}
+
+			this.createList();
+			return false;
+		},
+
+		createList: function() {
+			this.input.val(this.getElements().join(','));
+		},
+
+		onOpen: function() {
+			return false;
+		},
+
+		onAdd: function() {
+			return false;
+		}
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Medialist Widgets
+
+	sly.MedialistWidget = function(elem) {
+		sly.AbstractListWidget.call(this, elem);
+	};
+
+	inherit(sly.MedialistWidget, sly.AbstractListWidget);
+
+	sly.MedialistWidget.prototype.onOpen = function() {
+		sly.openMediapool('detail', this.getSelected(), 'slyMediaWidgetCallback');
+		slyMediaWidgetCallback = $.proxy(this.addValue, this);
+		return false;
+	};
+
+	sly.MedialistWidget.prototype.onAdd = function() {
+		sly.openMediapool('upload', '', 'slyMediaWidgetCallback');
+		slyMediaWidgetCallback = $.proxy(this.addValue, this);
+		return false;
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Linklist Widgets
+
+	sly.LinklistWidget = function(elem) {
+		sly.AbstractListWidget.call(this, elem);
+	};
+
+	inherit(sly.LinklistWidget, sly.AbstractListWidget);
+
+	sly.LinklistWidget.prototype.onOpen = function() {
+		var catID = this.element.data('catid');
+
+		sly.openLinkmap(catID, 'slyLinkWidgetCallback');
+		slyLinkWidgetCallback = $.proxy(this.addValue, this);
+
+		return false;
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Misc functions
+
+	sly.disableLogin = function(timerElement) {
+		var nextTime = parseInt(timerElement.html(), 10) - 1;
+
+		timerElement.html(nextTime + '');
+
+		if (nextTime > 0) {
+			setTimeout(sly.disableLogin, 1000, timerElement);
+		}
+		else {
+			$('div.rex-message p span').html($('#loginformular').data('message'));
+			$('#loginformular input:not(:hidden)').prop('disabled', false);
+			$('#rex_user_login').focus();
+		}
+	};
+
+	sly.startLoginTimer = function(message) {
+		var timerElement = $('div.rex-message p span strong');
+
+		if (timerElement.length == 1) {
+			$('#loginformular input:not(:hidden)').prop('disabled', true);
+			$('#loginformular').data('message', message);
+			setTimeout(sly.disableLogin, 1000, timerElement);
+		}
+	};
+
+	var catsChecked = function() {
+		var c_checked = $('#userperm_cat_all').prop('checked');
+		var m_checked = $('#userperm_media_all').prop('checked');
+		var slider    = $('#rex-page-user .sly-form .rex-form-wrapper .sly-num7');
+
+		$('#userperm_cat').prop('disabled', c_checked);
+		$('#userperm_media').prop('disabled', m_checked);
+
+		if (c_checked && m_checked)
+			slider.slideUp('slow');
+		else
+			slider.slideDown('slow');
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// dom:loaded handler
+
+	$(function() {
+		// Init widgets
+
+		$('.sly-widget').each(function() {
+			var self = $(this);
+
+			if (self.is('.sly-link')) {
+				new sly.LinkWidget(this);
+			}
+			else if (self.is('.sly-media')) {
+				new sly.MediaWidget(this);
+			}
+			else if (self.is('.sly-linklist')) {
+				new sly.LinklistWidget(this);
+			}
+			else if (self.is('.sly-medialist')) {
+				new sly.MedialistWidget(this);
+			}
+		});
+
+		// "check all" function for mediapool
+
+		$('.sly-check-all').click(function() {
+			var target = $(this).data('target');
+			$('input[name=\'' + target + '\']').prop('checked', this.checked);
+		});
+
+		// Lösch-Links in Tabellen
+
+		$('table.rex-table').delegate('a.sly-delete, input.sly-button-delete', 'click', function() {
+			var table    = $(this).parents('table');
+			var question = table.attr('rel');
+
+			if (!question) {
+				question = 'Löschen?';
+			}
+
+			return confirm(question);
+		});
+
+		// Filter-Funktionen in sly_Table
+
+		$('.sly-table-extras-filter input[class^=filter_input_]').keyup(function(event) {
+			// Klassen- und Tabellennamen ermitteln
+
+			var
+				className = $(this).attr('class'),
+				tableName = className.replace('filter_input_', ''),
+				table     = $('#' + tableName),
+				c         = event.keyCode;
+
+			// Wert auch in allen anderen Suchfeldern übernehmen
+
+			$('input.' + className).val($(this).val());
+
+			// Tabelle filtern
+
+			event.preventDefault();
+
+			if (c == 8 || c == 46 || c == 109 || c == 189 || (c >= 65 && c <= 90) || (c >= 48 && c <= 57)) {
+				var keyword = new RegExp($(this).val(), 'i');
+
+				$('tbody tr', table).each(function() {
+					var $tr = $(this);
+					$('td', $tr).filter(function() {
+						return keyword.test($(this).text());
+					}).length ? $tr.show() : $tr.hide();
+				});
+			}
+		});
+
+		// Links in neuem Fenster öffnen
+
+		$('a.sly-blank').attr('target', '_blank');
+
+	   // open mediapool in popup
+
+		$('#rex-navi-page-mediapool a').click(function() {
+			sly.openMediapool();
+			return false;
+		});
+
+		// Login-Formular
+
+		if ($('#rex-form-login').length > 0) {
+			$('#rex-form-login').focus();
+			$('#javascript').val('1');
+		}
+
+		// Benutzer-Formular
+
+		if ($('#rex-page-user .sly-form').length > 0) {
+			var wrapper = $('#rex-page-user .sly-form .rex-form-wrapper');
+			var sliders = wrapper.find('.sly-num6,.sly-num7');
+
+			$('#is_admin').change(function() {
+				if ($(this).is(':checked')) {
+					$('#userperm_module').prop('disabled', true);
+					sliders.slideUp('slow');
+				}
+				else {
+					$('#userperm_module').prop('disabled', false);
+					sliders.slideDown('slow');
+					catsChecked();
+				}
+			});
+
+			catsChecked();
+			$('#userperm_cat_all, #userperm_media_all').change(catsChecked);
+
+			// init behaviour
+
+			if ($('#is_admin').is(':checked')) {
+				$('#userperm_module').prop('disabled', true);
+				sliders.hide();
+			}
+
+			if ($('#userperm_cat_all').is(':checked') && $('#userperm_media_all').is(':checked')) {
+				wrapper.find('.sly-num7').hide();
+			}
+		}
+
+		// Formularframework
+
+		$('.rex-form .sly-select-checkbox-list a').live('click', function() {
+			var rel   = $(this).attr('rel');
+			var boxes = $(this).parents('p').find('.rex-chckbx');
+			boxes.prop('checked', rel === 'all');
+			return false;
+		});
+
+		// allen vom Browser unterstützten Elementen die Klasse ua-supported geben
+
+		if (typeof Modernizr !== 'undefined') {
+			var types = Modernizr.inputtypes;
+
+			for (var type in types) {
+				if (types.hasOwnProperty(type) && types[type]) {
+					$('input[type='+type+']').addClass('ua-supported');
+				}
+			}
+		}
+
+		// Fallback-Implementierung für type=range via jQuery UI Slider
+
+		$('input[type=range]:not(.ua-supported)').each(function() {
+			var input  = $(this);
+			var slider = $('<div></div>').attr('id', input.attr('id') + '-slider');
+			var hidden = $('<input type="hidden" value="" />');
+
+			// remove the old input element and replace it with a new, hidden one
+			input.after(hidden);
+			hidden.val(input.val()).attr('name', input.attr('name')).attr('id', input.attr('id'));
+
+			// create a new div that will be the slider
+			input.after(slider);
+			slider.addClass('sly-slider').slider({
+				min:    input.attr('min'),
+				max:    input.attr('max'),
+				value:  input.val(),
+				change: function(event) {
+					hidden.val(slider.slider('value'));
+				}
+			});
+
+			// remove it
+			input.remove();
+		});
+
+		// Mehrsprachige Formulare initialisieren
+
+		// Checkboxen erzeugen
+
+		$('.rex-form-row.sly-form-multilingual').each(function() {
+			var
+				$this   = $(this),
+				equal   = $this.is(':visible'),
+				id      = $this.attr('rel'),
+				checked = equal ? ' checked="checked"': '',
+				span    = '<span class="sly-form-i18n-switch"><input type="checkbox" name="equal__'+id+'" id="equal__'+id+'" value="1"'+checked+' /><\/span>';
+
+			if (equal) {
+				$('label:first', this).after(span);
+			}
+			else {
+				var container = $this.next('div.sly-form-i18n-container');
+				$('label:first', container).after(span);
+			}
+		});
+
+		// Checkboxen initialisieren
+
+		var checkboxes = $('.sly-form-i18n-switch input[id^=equal__]');
+
+		if (checkboxes.length > 0) {
+			checkboxes.imgCheckbox('off.png', 'on.png', 'assets/form-i18n-switch-').next().click(function() {
+				var
+					checkbox       = $(this).prev('input'),
+					shown          = !checkbox[0].checked, // was already changed before this event handler
+					id             = checkbox.attr('id').replace(/^equal__/, ''),
+					container      = $('div[rel="'+id+'"]'),
+					span           = checkbox.parent('span'),
+					equalcontainer = $('div.sly-form-i18n-container.c-'+id);
+
+				if (shown) {
+					container.hide();
+					equalcontainer.show();
+					$('label:first', equalcontainer).after(span);
+				}
+				else {
+					container.show();
+					equalcontainer.hide();
+					$('label:first', container).after(span);
+				}
+			});
+		}
+
+		// Module selection on content page
+
+		$('.sly-module-select').change(function() {
+			$(this).closest('form').submit();
+		});
+	});
+})(jQuery, sly);
---
+++ lib/Helper/Content.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * Copyright (c) 2011, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @author zozi@webvariants.de
+ */
+class sly_Helper_Content {
+
+	// ----- ADD Slice
+	public static function printAddSliceForm($prior, $module, $articleId, $clang, $slot, $values = array()) {
+		$moduleService = sly_Service_Factory::getModuleService();
+
+		if (!$moduleService->exists($module)) {
+			$slice_content = rex_warning(t('module_doesnt_exist'));
+		} else {
+			$moduleContent = $moduleService->getContent($moduleService->getInputFilename($module));
+			ob_start();
+			?>
+
+			<div class="sly-form" id="addslice">
+				<form action="index.php#slice<?= $prior ?>" id="slice<?= $prior ?>" method="post" enctype="multipart/form-data">
+					<div>
+						<input type="hidden" name="page" value="content" />
+						<input type="hidden" name="func" value="addArticleSlice" />
+						<input type="hidden" name="article_id" value="<?= $articleId ?>" />
+						<input type="hidden" name="clang" value="<?= $clang ?>" />
+						<input type="hidden" name="slot" value="<?= $slot ?>" />
+						<input type="hidden" name="module" value="<?= sly_html($module) ?>" />
+						<input type="hidden" name="prior" value="<?= $prior ?>" />
+					</div>
+					<fieldset class="rex-form-col-1">
+						<legend><?= t('add_block') ?>: <?= sly_html($moduleService->getTitle($module)) ?></legend>
+						<div class="rex-form-wrapper">
+							<div class="sly-contentpage-slice-input">
+								<? eval('?>' . self::replaceObjectVars($values, $moduleContent)); ?>
+							</div>
+							<div class="rex-form-row">
+								<p class="rex-form-submit">
+									<input type="submit" name="btn_save" value="<?= t('add_block') ?>" />
+								</p>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+
+			<?
+			self::focusFirstElement();
+
+			$slice_content = ob_get_clean();
+		}
+
+		print $slice_content;
+	}
+
+	public static function printEditSliceForm(OOArticleSlice $articleSlice, $values = array()) {
+		$moduleService = sly_Service_Factory::getModuleService();
+		ob_start();
+		?>
+		<a name="editslice"></a>
+
+		<div class="sly-form" id="editslice">
+			<form enctype="multipart/form-data" action="index.php#slice<?= $articleSlice->getPrior() ?>" method="post" id="REX_FORM">
+				<div>
+					<input type="hidden" name="page" value="content" />
+					<input type="hidden" name="func" value="editArticleSlice" />
+					<input type="hidden" name="article_id" value="<?= $articleSlice->getArticleId() ?>" />
+					<input type="hidden" name="clang" value="<?= $articleSlice->getClang() ?>" />
+					<input type="hidden" name="slice_id" value="<?= $articleSlice->getId() ?>" />
+					<input type="hidden" name="slot" value="<?= $articleSlice->getSlot() ?>" />
+					<input type="hidden" name="prior" value="<?= $articleSlice->getPrior() ?>" />
+				</div>
+				<fieldset class="rex-form-col-1">
+					<legend><?= t('edit_block') ?>: <?= sly_html($moduleService->getTitle($articleSlice->getModule())) ?></legend>
+					<div class="rex-form-wrapper">
+						<div class="sly-contentpage-slice-input">
+							<? eval('?>' . self::replaceObjectVars($values, $articleSlice->getInput())); ?>
+						</div>
+						<div class="rex-form-row">
+							<p class="rex-form-submit">
+								<input class="rex-form-submit" type="submit" value="<?= t('save_block') ?>" name="btn_save" />
+								<input class="rex-form-submit rex-form-submit-2" type="submit" value="<?= t('update_block') ?>" name="btn_update" />
+							</p>
+						</div>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+		<?
+		self::focusFirstElement();
+
+		$slice_content = ob_get_clean();
+		print $slice_content;
+	}
+
+	/**
+	 * Perform REX_VAR replacements
+	 *
+	 * @param  int    $slice_id  the slice's ID
+	 * @param  string $content   current slice content
+	 * @return string            parsed content
+	 */
+	private static function replaceObjectVars($data, $content) {
+		foreach (sly_Core::getVarTypes() as $idx => $var) {
+			$content = $var->getBEInput($data, $content);
+		}
+		return $content;
+	}
+
+	private static function focusFirstElement() {
+		$layout = sly_Core::getLayout();
+		$layout->addJavaScript('jQuery(function($) { $("#addslice, #editslice").find(":input:visible:enabled:not([readonly]):first").focus(); });');
+	}
+
+	public static function metaFormAddButtonBar($form, $label, $name) {
+		$button = new sly_Form_Input_Button('submit', $name, $label);
+		$button->setAttribute('onclick', 'return confirm(\'' . $label . '?\')');
+		$form->add(new sly_Form_ButtonBar(array('submit' => $button)));
+	}
+
+}
