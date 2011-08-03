275fb7fc02fc5c381e02b1cdc42707cc742a8d8d
Zozi <zozfried@users.noreply.github.com>
2011-08-04 00:18:25 +0200

    there will be no rex compatible addons, remove the sally indicators

---
+++ lib/Controller/Specials.php
@@ -0,0 +1,136 @@
+<?php
+/*
+ * Copyright (c) 2011, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+class sly_Controller_Specials extends sly_Controller_Backend {
+	protected $warning;
+	protected $info;
+
+	protected function init() {
+		$subline = array(
+			array('',          t('main_preferences')),
+			array('languages', t('languages'))
+		);
+
+		// add subpages
+
+		$navigation = sly_Core::getNavigation();
+		$specials   = $navigation->get('specials', 'system');
+
+		$specials->addSubpage('', t('main_preferences'));
+		$specials->addSubpage('languages', t('languages'));
+
+		// show error log when using the original system error handler
+		// (that's the only case when we can ensure that we're able to parse the error log)
+
+		if (!sly_Core::isDeveloperMode()) {
+			$handler = sly_Core::getErrorHandler();
+
+			if (get_class($handler) === 'sly_ErrorHandler_Production') {
+				$specials->addSubpage('errorlog', t('errorlog'));
+				$subline[] = array('errorlog',  t('errorlog'));
+			}
+		}
+
+		$layout = sly_Core::getLayout();
+		$layout->pageHeader(t('specials'), $subline);
+	}
+
+	protected function index() {
+		print $this->render('specials/index.phtml');
+	}
+
+	protected function clearcache() {
+		$this->info = rex_generateAll();
+		$this->index();
+	}
+
+	protected function update() {
+		$startArticle    = sly_post('start_article',    'int');
+		$notFoundArticle = sly_post('notfound_article', 'int');
+		$defaultClang    = sly_post('default_clang',    'int');
+		$defaultType     = sly_post('default_type',     'string');
+		$developerMode   = sly_post('developer_mode',   'string');
+		$backendLocale   = sly_post('backend_locale',   'string');
+		$projectName     = sly_post('projectname',      'string');
+		$cachingStrategy = sly_post('caching_strategy', 'string');
+		$timezone        = sly_post('timezone',         'string');
+
+		// Änderungen speichern
+
+		$conf = sly_Core::config();
+		$this->warning = array();
+
+		if (sly_Util_Article::exists($startArticle)) {
+			$conf->set('START_ARTICLE_ID', $startArticle);
+		}
+		else {
+			$this->warning[] = t('settings_invalid_sitestart_article');
+		}
+
+		if (sly_Util_Article::exists($notFoundArticle)) {
+			$conf->set('NOTFOUND_ARTICLE_ID', $notFoundArticle);
+		}
+		else {
+			$this->warning[] = t('settings_invalid_notfound_article').'<br />';
+		}
+
+		if (sly_Util_Language::exists($defaultClang)) {
+			$conf->set('DEFAULT_CLANG_ID', $defaultClang);
+		}
+		else {
+			$this->warning[] = t('settings_invalid_sitestart_clang').'<br />';
+		}
+
+		// Standard-Artikeltyp
+
+		try {
+			$service = sly_Service_Factory::getArticleTypeService();
+
+			if (!empty($defaultType) && !$service->exists($defaultType)) {
+				$this->warning[] = t('settings_invalid_default_type').'<br />';
+			}
+			else {
+				$conf->set('DEFAULT_ARTICLE_TYPE', $defaultType);
+			}
+		}
+		catch (Exception $e) {
+			$conf->set('DEFAULT_ARTICLE_TYPE', '');
+		}
+
+		// Sonstige Einstellungen
+
+		$conf->set('DEVELOPER_MODE', $developerMode === 'true');
+		$conf->set('DEFAULT_LOCALE', $backendLocale);
+		$conf->set('PROJECTNAME', $projectName);
+		$conf->setLocal('CACHING_STRATEGY', $cachingStrategy);
+
+		if (class_exists('DateTimeZone')) {
+			$conf->set('TIMEZONE', $timezone);
+		}
+
+		$this->info    = t('info_updated');
+		$this->warning = implode("<br />\n", $this->warning);
+
+		// notify system
+		sly_Core::dispatcher()->notify('SLY_SETTINGS_UPDATED');
+
+		$this->index();
+	}
+
+	protected function setup() {
+		sly_Core::config()->setLocal('SETUP', true);
+		sly_Util_HTTP::redirect('index.php', array(), '', 302);
+	}
+
+	protected function checkPermission() {
+		$user = sly_Util_User::getCurrentUser();
+		return $user && $user->isAdmin();
+	}
+}
---
+++ lib/Layout/Backend.php
@@ -0,0 +1,181 @@
+<?php
+/*
+ * Copyright (c) 2011, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @ingroup layout
+ */
+class sly_Layout_Backend extends sly_Layout_XHTML {
+	private $hasNavigation = true;
+
+	public function __construct() {
+		$config = sly_Core::config();
+
+		$this->addCSSFile('assets/css/import.css');
+
+		$this->addJavaScriptFile('assets/js/jquery.min.js');
+		$this->addJavaScriptFile('assets/js/standard.min.js');
+		$this->addJavaScriptFile('assets/js/modernizr.min.js');
+
+		$this->setTitle(sly_Core::getProjectName().' - ');
+
+		$config = sly_Core::config();
+		$this->addMeta('robots', 'noindex,nofollow');
+
+		sly_Core::dispatcher()->register('PAGE_CHECKED', array($this, 'pageChecked'));
+	}
+
+	public function pageChecked(array $params) {
+		$body_id = str_replace('_', '-', $params['subject']);
+		$this->setBodyAttr('id', 'rex-page-'.$body_id);
+
+		$popups_arr = array('linkmap', 'mediapool');
+
+		if (in_array($body_id, $popups_arr)) {
+			$this->setBodyAttr('class', 'rex-popup');
+		}
+
+		$active = sly_Core::getNavigation()->getActivePage();
+
+		if ($active && $active->isPopup()) {
+			$this->setBodyAttr('onunload', 'sly.closeAllPopups()');
+		}
+	}
+
+	public function printHeader() {
+		parent::printHeader();
+		print $this->renderView('layout/sally/top.phtml');
+	}
+
+	public function printFooter() {
+		print $this->renderView('layout/sally/bottom.phtml');
+		parent::printFooter();
+	}
+
+	public function pageHeader($head, $subtitle = '') {
+		if (!empty($subtitle)) {
+			$subtitle = '<div class="pagehead-row">'.$this->getSubtitle($subtitle).'</div>';
+		}
+
+		$this->appendToTitle($head);
+		$dispatcher = sly_Core::dispatcher();
+
+		$page = sly_Core::getCurrentPage();
+		$head = $dispatcher->filter('PAGE_TITLE', $head, compact('page'));
+		print '<div id="sly-pagehead"><div class="pagehead-row"><h1>'.$head.'</h1></div>'.$subtitle.'</div>';
+
+		$dispatcher->notify('PAGE_TITLE_SHOWN', $subtitle, compact('page'));
+	}
+
+	/**
+	 * Helper function, die den Subtitle generiert
+	 */
+	public function getSubtitle($subline, $attr = '') {
+		if (empty($subline)) {
+			return '';
+		}
+
+		$subtitle_str = $subline;
+		$subtitle     = $subline;
+		$cur_subpage  = sly_request('subpage', 'string');
+		$cur_page     = urlencode(sly_request('page', 'string'));
+		$user         = sly_Util_User::getCurrentUser();
+
+		if (is_array($subline) && !empty($subline)) {
+			$subtitle = array();
+			$numPages = count($subline);
+			$isAdmin  = $user->isAdmin();
+
+			foreach ($subline as $subpage) {
+				if (!is_array($subpage)) {
+					continue;
+				}
+
+				$link   = $subpage[0];
+				$label  = $subpage[1];
+				$perm   = !empty($subpage[2]) ? $subpage[2] : '';
+				$params = !empty($subpage[3]) ? sly_Util_HTTP::queryString($subpage[3]) : '';
+
+				// Berechtigung prüfen
+				// Hat der User das Recht für die aktuelle Subpage?
+
+				if (!empty($perm) && !$isAdmin && !$user->hasRight($perm)) {
+					// Wenn der User kein Recht hat, und diese Seite öffnen will -> Fehler
+					if ($cur_subpage == $link) {
+						exit('You have no permission to this area!');
+					}
+					// Den Punkt aus der Navi entfernen
+					else {
+						continue;
+					}
+				}
+
+				$link   = explode('&', $link, 2);
+				$link   = reset($link); // alles nach dem ersten & abschneiden
+				$active = (empty($cur_subpage) && empty($link)) || (!empty($cur_subpage) && $cur_subpage == $link);
+
+				// Auf der aktiven Seite den Link nicht anzeigen
+				if ($active) {
+					$link       = empty($link) ? '' : '&amp;subpage='.urlencode($link);
+					$format     = '<a href="?page='.$cur_page.'%s%s"%s class="rex-active">%s</a>';
+					$subtitle[] = sprintf($format, $link, $params, $attr, $label);
+				}
+				elseif (empty($link)) {
+					$format     = '<a href="?page='.$cur_page.'%s"%s>%s</a>';
+					$subtitle[] = sprintf($format, $params, $attr, $label);
+				}
+				else {
+					$link       = '&amp;subpage='.urlencode($link);
+					$format     = '<a href="?page='.$cur_page.'%s%s"%s>%s</a>';
+					$subtitle[] = sprintf($format, $link, $params, $attr, $label);
+				}
+			}
+
+			if (!empty($subtitle)) {
+				$items = array();
+				$i     = 1;
+
+				foreach ($subtitle as $part) {
+					if ($i == 1) {
+						$items[] = '<li class="rex-navi-first">'.$part.'</li>';
+					}
+					else {
+						$items[] = '<li>'.$part.'</li>';
+					}
+
+					++$i;
+				}
+
+				$subtitle_str = '<div id="rex-navi-page"><ul>'.implode("\n", $items).'</ul></div>';
+			}
+		}
+
+		return $subtitle_str;
+	}
+
+	/**
+	 * override default hasNavigation value
+	 *
+	 * @param boolean $active true to show navigation falso to hide
+	 */
+	public function showNavigation($active = true) {
+		$this->hasNavigation = $active;
+	}
+
+	public function hasNavigation() {
+		return $this->hasNavigation;
+	}
+
+	protected function getViewFile($file) {
+		$full = SLY_SALLYFOLDER.'/backend/views/'.$file;
+		if (file_exists($full)) return $full;
+
+		return parent::getViewFile($file);
+	}
+}
