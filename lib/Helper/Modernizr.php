891ee63a4b02e7c6ad386a13d176651420cca904
Zozi <zozfried@users.noreply.github.com>
2012-09-10 21:12:25 +0200

    drop $this->action

---
+++ lib/Helper/Modernizr.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+class sly_Helper_Modernizr {
+	private static $data = null;
+
+	public static function hasCapability($test) {
+		$info = self::getCapabilities();
+		return isset($info[$test]) && $info[$test] === true;
+	}
+
+	public static function getCapabilities() {
+		if (self::$data === null) {
+			$cookie = isset($_COOKIE['sly_modernizr']) ? $_COOKIE['sly_modernizr'] : 'false';
+			$cookie = @json_decode($cookie, true);
+
+			self::$data = $cookie;
+		}
+
+		return self::$data;
+	}
+
+	public static function hasInputtype($type) {
+		$data = self::getCapabilities();
+		return !empty($data['inputtypes'][$type]);
+	}
+
+	public static function hasInput($type) {
+		$data = self::getCapabilities();
+		return !empty($data['input'][$type]);
+	}
+}
