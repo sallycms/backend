0d7afa48451ce53f1d196d1596548f9780bc9172
Zozi <zozfried@users.noreply.github.com>
2012-10-01 11:45:55 +0200

    add API function to check for SETUP, and hardcode the default value

---
+++ lib/Controller/Content.php
@@ -0,0 +1,319 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+class sly_Controller_Content extends sly_Controller_Content_Base {
+	protected $localMessages = false;
+
+	public function indexAction($extraparams = array()) {
+		$this->init();
+		if ($this->header() !== true) return;
+
+		$service = sly_Service_Factory::getArticleTypeService();
+		$types   = $service->getArticleTypes();
+		$modules = array();
+
+		if ($this->article->hasType()) {
+			try {
+				$modules = $service->getModules($this->article->getType(), $this->slot);
+			}
+			catch (Exception $e) {
+				$modules = array();
+			}
+		}
+
+		foreach ($modules as $idx => $module) $modules[$idx] = sly_translate($module);
+		foreach ($types as $idx => $type)     $types[$idx]   = sly_translate($type);
+
+		uasort($types, 'strnatcasecmp');
+		uasort($modules, 'strnatcasecmp');
+
+		$params = array(
+			'article'      => $this->article,
+			'articletypes' => $types,
+			'modules'      => $modules,
+			'slot'         => $this->slot,
+			'slice_id'     => sly_request('slice_id', 'int', 0),
+			'pos'          => sly_request('pos', 'int', 0),
+			'function'     => sly_request('function', 'string'),
+			'module'       => sly_request('add_module', 'string')
+		);
+
+		$params = array_merge($params, $extraparams);
+		$this->render('content/index.phtml', $params, false);
+	}
+
+	protected function getPageName() {
+		return 'content';
+	}
+
+	public function checkPermission($action, $forceModule = null) {
+		$this->action = $action;
+
+		if (parent::checkPermission($this->action)) {
+			$user = sly_Util_User::getCurrentUser();
+
+			if ($this->action === 'moveslice') {
+				$slice_id = sly_request('slice_id', 'int', null);
+
+				if ($slice_id) {
+					$slice = sly_Util_ArticleSlice::findById($slice_id);
+					return ($user->isAdmin() || $user->hasRight('module', 'delete', $slice->getModule()));
+				}
+
+				return false;
+			}
+
+			if ($action === 'addarticleslice') {
+				$module = $forceModule === null ? sly_request('module', 'string') : $forceModule;
+				return ($user->isAdmin() || $user->hasRight('module', 'add', sly_Authorisation_ModuleListProvider::ALL) || $user->hasRight('module', 'add', $module));
+			}
+
+			if ($action === 'editarticleslice') {
+				// skip the slice stuff if the user is admin
+				if ($user->isAdmin()) return true;
+
+				if ($forceModule === null) {
+					$sliceservice = sly_Service_Factory::getArticleSliceService();
+					$slice_id     = sly_request('slice_id', 'int', 0);
+					$slice        = $sliceservice->findById($slice_id);
+					$module       = $slice->getModule();
+				}
+				else {
+					$module = $forceModule;
+				}
+
+				return $user->hasRight('module', 'edit', sly_Authorisation_ModuleListProvider::ALL) || $user->hasRight('module', 'edit', $module);
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public function setarticletypeAction() {
+		$this->init();
+
+		$type    = sly_post('article_type', 'string');
+		$service = sly_Service_Factory::getArticleService();
+		$flash   = sly_Core::getFlashMessage();
+
+		// change type and update database
+		$service->setType($this->article, $type);
+		$flash->appendInfo(t('article_updated'));
+
+		$this->article = $service->findById($this->article->getId());
+		$this->indexAction();
+	}
+
+	public function movesliceAction() {
+		$this->init();
+
+		$slice_id  = sly_get('slice_id', 'int');
+		$direction = sly_get('direction', 'string');
+		$flash     = sly_Core::getFlashMessage();
+
+		// check of module exists
+		$module = sly_Util_ArticleSlice::getModule($slice_id);
+
+		if (!$module) {
+			$flash->appendWarning(t('module_not_found'));
+		}
+		else {
+			$user = sly_Util_User::getCurrentUser();
+
+			// check permission
+			if ($user->isAdmin() || $user->hasRight('module', 'move', $module) || $user->hasRight('module', 'move', sly_Authorisation_ModuleListProvider::ALL)) {
+				$success = sly_Service_Factory::getArticleSliceService()->move($slice_id, $direction);
+				$this->localMessages = true;
+
+				if ($success) {
+					$flash->appendInfo(t('slice_moved'));
+				}
+				else {
+					$flash->appendWarning(t('cannot_move_slice'));
+				}
+			}
+			else {
+				$flash->appendWarning(t('no_rights_to_this_module'));
+			}
+		}
+
+		$this->indexAction();
+	}
+
+	public function addarticlesliceAction() {
+		$this->init();
+
+		$module    = sly_post('module', 'string');
+		$params    = array();
+		$slicedata = $this->preSliceEdit('add');
+		$flash     = sly_Core::getFlashMessage();
+
+		if ($slicedata['SAVE'] === true) {
+			$service  = sly_Service_Factory::getArticleSliceService();
+			$pos      = sly_post('pos', 'int', 0);
+			$instance = $service->add($this->article, $this->slot, $module, $slicedata['VALUES'], $pos);
+
+			$this->localMessages = true;
+			$flash->appendInfo(t('slice_added'));
+
+			$this->postSliceEdit('add', $instance->getId());
+		}
+		else {
+			$params['function']    = 'add';
+			$params['module']      = $module;
+			$params['slicevalues'] = $this->getRequestValues(array());
+		}
+
+		$this->indexAction($params);
+	}
+
+	public function editarticlesliceAction() {
+		$this->init();
+
+		$articleSliceService = sly_Service_Factory::getArticleSliceService();
+		$sliceService        = sly_Service_Factory::getSliceService();
+		$slice_id            = sly_request('slice_id', 'int', 0);
+		$articleSlice        = $articleSliceService->findById($slice_id);
+		$flash               = sly_Core::getFlashMessage();
+
+		$slicedata = $this->preSliceEdit('edit');
+
+		if ($slicedata['SAVE'] === true) {
+			$slice = $articleSlice->getSlice();
+			$slice->setValues($slicedata['VALUES']);
+			$sliceService->save($slice);
+
+			$articleSlice->setUpdateColumns();
+			$articleSliceService->save($articleSlice);
+
+			$this->localMessages = true;
+			$flash->appendInfo(t('slice_updated'));
+			$this->postSliceEdit('edit', $slice_id);
+		}
+
+		$extraparams = array();
+
+		if (sly_post('btn_update', 'string') || $slicedata['SAVE'] !== true) {
+			$extraparams['slicevalues'] = $slicedata['VALUES'];
+			$extraparams['function']    = 'edit';
+		}
+
+		$this->indexAction($extraparams);
+	}
+
+	public function deletearticlesliceAction() {
+		$this->init();
+
+		$ok      = false;
+		$sliceID = sly_request('slice_id', 'int', 0);
+		$slice   = sly_Util_ArticleSlice::findById($sliceID);
+		$flash   = sly_Core::getFlashMessage();
+
+		$this->localMessages = true;
+
+		if (!$slice) {
+			$flash->appendWarning(t('module_not_found', $sliceID));
+			return $this->indexAction();
+		}
+
+		$module = $slice->getModule();
+		$user   = sly_Util_User::getCurrentUser();
+
+		if (!$user->isAdmin() && !$user->hasRight('module', 'edit', sly_Authorisation_ModuleListProvider::ALL) && !$user->hasRight('module', 'edit', $module)) {
+			$flash->appendWarning(t('no_rights_to_this_module'));
+			return $this->indexAction();
+		}
+
+		if ($this->preSliceEdit('delete') !== false) {
+			$ok = sly_Util_ArticleSlice::deleteById($sliceID);
+		}
+
+		if ($ok) {
+			$flash->appendInfo(t('slice_deleted'));
+			$this->postSliceEdit('delete', $sliceID);
+		}
+		else {
+			$flash->appendWarning(t('cannot_delete_slice'));
+		}
+
+		$this->indexAction();
+	}
+
+	private function preSliceEdit($function) {
+		if (!$this->article->hasTemplate()) return false;
+
+		if ($function == 'delete' || $function == 'edit') {
+			$slice_id = sly_request('slice_id', 'int', 0);
+			if (!sly_Util_ArticleSlice::exists($slice_id)) return false;
+			$module = sly_Util_ArticleSlice::getModuleNameForSlice($slice_id );
+		}
+		else {
+			$module = sly_post('module', 'string');
+		}
+
+		$flash = sly_Core::getFlashMessage();
+
+		if ($function !== 'delete') {
+			if (!sly_Service_Factory::getModuleService()->exists($module)) {
+				$flash->appendWarning(t('module_not_found'));
+				return false;
+			}
+
+			if (!sly_Service_Factory::getArticleTypeService()->hasModule($this->article->getType(), $module, $this->slot)) {
+				$slotTitle  = $templateService->getSlotTitle($templateName, $this->slot);
+				$moduleName = sly_Service_Factory::getModuleService()->getTitle($module);
+
+				$flash->appendWarning(t('module_not_allowed_in_slot', $moduleName, $slotTitle));
+				return false;
+			}
+		}
+
+		// Daten einlesen
+		$slicedata = array('SAVE' => true);
+
+		if ($function != 'delete') {
+			$slicedata = $this->getRequestValues($slicedata);
+		}
+
+		// ----- PRE SAVE EVENT [ADD/EDIT/DELETE]
+		$eventparams = array('module' => $module, 'article_id' => $this->article->getId(), 'clang' => $this->article->getClang());
+		$slicedata   = sly_Core::dispatcher()->filter('SLY_SLICE_PRESAVE_'.strtoupper($function), $slicedata, $eventparams);
+
+		// don't save
+		if (!$slicedata['SAVE']) {
+			if ($this->action == 'deleteArticleSlice') {
+				$flash->appendWarning(t('cannot_delete_slice'));
+			}
+			else {
+				$flash->prependWarning(t('cannot_update_slice'));
+			}
+		}
+
+		return $slicedata;
+	}
+
+	private function postSliceEdit($function, $articleSliceId) {
+		$user       = sly_Util_User::getCurrentUser();
+		$flash      = sly_Core::getFlashMessage();
+		$dispatcher = sly_Core::dispatcher();
+
+		sly_Service_Factory::getArticleService()->touch($this->article, $user);
+
+		$dispatcher->notify('SLY_SLICE_POSTSAVE_'.strtoupper($function), $articleSliceId);
+		$dispatcher->notify('SLY_CONTENT_UPDATED', $this->article, array('article_id' => $this->article->getId(), 'clang' => $this->article->getClang()));
+	}
+
+	private function getRequestValues(array $slicedata) {
+		$slicedata['VALUES'] = sly_post('slicevalue', 'array', array());
+		return $slicedata;
+	}
+}
---
+++ lib/Helper/Content.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @author zozi@webvariants.de
+ */
+class sly_Helper_Content {
+
+	// ----- ADD Slice
+	public static function printAddSliceForm($module, $position, $articleId, $clang, $slot) {
+		$moduleService = sly_Service_Factory::getModuleService();
+
+		if (!$moduleService->exists($module)) {
+			$slice_content = sly_Helper_Message::warn(ht('module_not_found', $module));
+		}
+		else {
+			try {
+				ob_start();
+				$moduleTitle = $moduleService->getTitle($module);
+				$form = new sly_Form('index.php#slice'.$position, 'post', t('add_slice').': '.sly_translate($moduleTitle, true), '', 'slice'.$position);
+				$form->setEncType('multipart/form-data');
+				$form->addHiddenValue('page', 'content');
+				$form->addHiddenValue('func', 'addArticleSlice');
+				$form->addHiddenValue('article_id', $articleId);
+				$form->addHiddenValue('clang', $clang);
+				$form->addHiddenValue('slot', $slot);
+				$form->addHiddenValue('module', sly_html($module));
+				$form->addHiddenValue('pos', $position);
+				$form->setSubmitButton(new sly_Form_Input_Button('submit', 'btn_save', t('add_slice')));
+
+				$renderer   = new sly_Slice_Renderer($module);
+				$sliceinput = new sly_Form_Fragment();
+				$sliceinput->setContent('<div class="sly-contentpage-slice-input">'.$renderer->renderInput('slicevalue').'</div>');
+
+				$form->add($sliceinput);
+				$form->addClass('sly-slice-form');
+
+				print $form->render();
+
+				self::focusFirstElement();
+
+				sly_Core::dispatcher()->notify('SLY_SLICE_POSTVIEW_ADD', array(), array(
+					'module'     => $module,
+					'article_id' => $articleId,
+					'clang'      => $clang,
+					'slot'       => $slot
+				));
+
+				$slice_content = ob_get_clean();
+			}
+			catch (Exception $e) {
+				ob_end_clean();
+				throw $e;
+			}
+		}
+
+		print $slice_content;
+	}
+
+	public static function printEditSliceForm(sly_Model_ArticleSlice $articleSlice, $values = array()) {
+		$moduleService = sly_Service_Factory::getModuleService();
+		$module        = $articleSlice->getModule();
+		$moduleTitle   = $moduleService->getTitle($module);
+
+		try {
+			ob_start();
+			$form = new sly_Form('index.php#slice'.$articleSlice->getPosition(), 'post', t('edit_slice').': '.sly_translate($moduleTitle, true));
+			$form->setEncType('multipart/form-data');
+			$form->addHiddenValue('page', 'content');
+			$form->addHiddenValue('func', 'editArticleSlice');
+			$form->addHiddenValue('article_id', $articleSlice->getArticleId());
+			$form->addHiddenValue('clang', $articleSlice->getClang());
+			$form->addHiddenValue('slice_id', $articleSlice->getId());
+			$form->addHiddenValue('slot', $articleSlice->getSlot());
+			$form->setSubmitButton(new sly_Form_Input_Button('submit', 'btn_save', t('save')));
+			$form->setApplyButton(new sly_Form_Input_Button('submit', 'btn_update', t('apply')));
+			$form->setResetButton(new sly_Form_Input_Button('reset', 'reset', t('reset')));
+
+			$renderer   = new sly_Slice_Renderer($module, $values);
+			$sliceinput = new sly_Form_Fragment();
+			$sliceinput->setContent('<div class="sly-contentpage-slice-input">'.$renderer->renderInput('slicevalue').'</div>');
+
+			$form->add($sliceinput);
+			$form->addClass('sly-slice-form');
+
+			print $form->render();
+
+			self::focusFirstElement();
+
+			sly_Core::dispatcher()->notify('SLY_SLICE_POSTVIEW_EDIT', $values, array(
+				'module'     => $articleSlice->getModule(),
+				'article_id' => $articleSlice->getArticleId(),
+				'clang'      => $articleSlice->getClang(),
+				'slot'       => $articleSlice->getSlot(),
+				'slice'      => $articleSlice
+			));
+
+			$slice_content = ob_get_clean();
+		}
+		catch (Exception $e) {
+			ob_end_clean();
+			throw $e;
+		}
+
+		print $slice_content;
+	}
+
+	private static function focusFirstElement() {
+		$layout = sly_Core::getLayout();
+		$layout->addJavaScript('jQuery(function($) { $(".sly-slice-form").find(":input:visible:enabled:not([readonly]):first").focus(); });');
+	}
+
+	public static function metaFormAddButtonBar($form, $label, $name) {
+		$button = new sly_Form_Input_Button('submit', $name, $label);
+		$button->setAttribute('onclick', 'return confirm('.json_encode($label.'?').')');
+		$form->add(new sly_Form_ButtonBar(array('submit' => $button)));
+	}
+}
