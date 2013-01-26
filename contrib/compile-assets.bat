6dbbd17ef5bdd4ab8f72459bc29f2fa787138cd7
Christoph <github@xrstf.de>
2013-01-26 21:41:12 +0100

    re-added a script to compile our assets, updated jQuery to 1.9.0, updated json2

---
+++ contrib/compile-assets.bat
@@ -0,0 +1,5 @@
+@echo off
+call uglifyjs ../assets/js/json2.js -nc -m -c -o ../assets/js/json2.min.js
+call uglifyjs ../assets/js/standard.js -nc -m -c -o ../assets/js/standard.min.js
+call uglifyjs ../assets/js/locales/de_de.js -nc -m -c -o ../assets/js/locales/de_de.min.js
+call uglifyjs ../assets/js/locales/en_gb.js -nc -m -c -o ../assets/js/locales/en_gb.min.js
